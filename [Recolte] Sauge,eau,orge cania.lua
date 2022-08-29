7e6cea36e7751d16f843d5c2139d68a665e3da5d
Arthur.B <48888351+Sehyn@users.noreply.github.com>
2022-08-29 10:35:17 +0200

    Hotfix sauge
    ID: 255

--- [Recolte] Sauge,eau,orge cania.lua
+++ [Recolte] Sauge,eau,orge cania.lua
@@ -1,5 +1,5 @@
 MAX_PODS = 90
-ELEMENTS_TO_GATHER = {84,43,44,46,47,38,35}
+ELEMENTS_TO_GATHER = {84,43,44,46,47,38,35,255}
 botname = character.name()
 botbucheronlevel = job:level(2)
 botalchimistelevel = job:level(26)
